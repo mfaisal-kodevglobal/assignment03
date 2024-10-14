f825818bdc32a90d91f856279ef029211989cdcb
muhammad Faisal <muhammad.faisal.ms23@gmail.com>
2024-10-14 23:14:01 +0500

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,30 +4,34 @@ import Footer from "./cmoponents/footer/page";
 
 export default function Home() {
   return (
-    <div style={{'background':'#f39ff3', 'display':'flex'}} className="grid grid-rows-[20px_1fr_20px]  min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Header />
-      <div style={{'width':'55%','padding':'130px','fontSize': '30px', 'border':'1px solid gray', 'marginRight':'20px', 'marginLeft':'10px'}}>
-        
-        <h1><u>Muhammad Faisal</u></h1>
-        <div>
-            <p>I am a new nextjs developer</p>
-            <p>I am a new nextjs developer</p>
-            <p>I am a new nextjs developer</p>
-            <p>I am a new nextjs developer</p>
-            <p>I am a new nextjs developer</p>
+    <>
+    <Header />
+    <div style={{'background':'#f39ff3', 'marginTop':'65px'}} className="grid grid-rows-[20px_1fr_20px]  min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      
+      <div style={{'display':'flex'}}>
+        <div style={{'width':'55%','padding':'100px','fontSize': '30px', 'border':'1px solid gray', 'marginRight':'20px', 'marginLeft':'10px'}}>
           
+          <h1><u>Muhammad Faisal</u></h1>
+          <div>
+              <p>I am a new nextjs developer</p>
+              <p>I am a new nextjs developer</p>
+              <p>I am a new nextjs developer</p>
+              <p>I am a new nextjs developer</p>
+              <p>I am a new nextjs developer</p>
             
-            
+              
+              
+          </div>
         </div>
-      </div>
-      <div style={{'width':'25%','padding':'180px', 'border':'1px solid gray'}}>
-      <img
-            aria-hidden
-            src="https://i.pinimg.com/564x/1b/48/f8/1b48f8dd121c97ac9690921b441317a8.jpg"
-            alt="File icon"
-            width={400}
-            height={400}
-          />
+        <div style={{'width':'25%','padding':'100px', 'border':'1px solid gray'}}>
+        <img
+              aria-hidden
+              src="https://i.pinimg.com/564x/1b/48/f8/1b48f8dd121c97ac9690921b441317a8.jpg"
+              alt="File icon"
+              width={400}
+              height={400}
+            />
+        </div>
       </div>
       <Footer />
       {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
@@ -78,5 +82,7 @@ export default function Home() {
         </a>
       </footer> */}
     </div>
+    </>
+    
   );
 }
